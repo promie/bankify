830645499fd15824ce6f9affe80405987664521a
Promie Yutasane <promie.yutasane@gmail.com>
2025-08-02 20:38:24 +1000

    added logic to main file on entry point

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import accounts.BankAccount;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        BankAccount account = new BankAccount("123456", "Promie");
+
+        // Test deposit money
+        account.deposit(500);
+
+        // Test Withdrawal
+        account.withdraw(250);
+
+        // Print account info
+        System.out.println(account.getAccountInfo());
     }
 }
